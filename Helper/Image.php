09dad641c20b9e65604d4397e6d3de980151d454
Murilo K S Melo <53019759+muriloksmelo@users.noreply.github.com>
2025-08-20 19:35:41 -0300

    1.6.1
    1.6.1 - ajuste ao verificar diretório de imagem.

--- Helper/Image.php
+++ Helper/Image.php
@@ -1,9 +1,13 @@
 <?php
 namespace Pagcommerce\Payment\Helper;
+
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\App\Helper\Context;
+use Magento\Framework\App\Filesystem\DirectoryList;
 
 class Image extends AbstractHelper
 {
@@ -12,15 +16,23 @@ class Image extends AbstractHelper
      */
     private $storeManager;
 
+    protected DirectoryList $directoryList;
+
     public function __construct(
         Context                $context,
-        StoreManagerInterface  $storeManager
+        StoreManagerInterface  $storeManager,
+        DirectoryList $directoryList
     ) {
         parent::__construct($context);
         $this->storeManager = $storeManager;
+        $this->directoryList = $directoryList;
     }
 
 
+    /**
+     * @throws NoSuchEntityException
+     * @throws FileSystemException
+     */
     public function convertBase64ToImage($b64, \Magento\Sales\Model\Order $order){
         $b64 = str_replace('data:image/png;base64,', '', $b64);
         $bin = base64_decode($b64);
@@ -31,10 +43,18 @@ class Image extends AbstractHelper
 
         $imgName = $order->getId().'-'.$order->getIncrementId().'-'.$order->getCustomerId().'.png';
 
-        $imageSavePath = 'media'.DIRECTORY_SEPARATOR.'pagcommerce_payment';
-        if(!is_dir($imageSavePath)){
-            mkdir($imageSavePath);
+        // Caminho absoluto até pub/media/pagcommerce_payment
+        $mediaDir = $this->directoryList->getPath(DirectoryList::MEDIA);
+        $imageSavePath  = $mediaDir . DIRECTORY_SEPARATOR . 'pagcommerce_payment';
+
+        if (!is_dir($imageSavePath)) {
+            mkdir($imageSavePath, 0775, true); // cria com permissão e recursivo
         }
+
+        //$imageSavePath = 'media'.DIRECTORY_SEPARATOR.'pagcommerce_payment';
+        //if(!is_dir($imageSavePath)){
+            //mkdir($imageSavePath);
+        //}
 
         $imageSavePath = $imageSavePath.DIRECTORY_SEPARATOR.$imgName;
         if(!is_file($imageSavePath)){
